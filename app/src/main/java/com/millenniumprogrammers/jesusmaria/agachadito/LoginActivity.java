e51cedd835d92f25dfa39d4b9b4379f02ea61ac0
CesarsEren <cpinedo428@gmail.com>
2019-12-03 16:40:30 -0500

    Primer Commit

---
+++ app/src/main/java/com/millenniumprogrammers/jesusmaria/agachadito/LoginActivity.java
@@ -0,0 +1,27 @@
+package com.millenniumprogrammers.jesusmaria.agachadito;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        btn = findViewById(R.id.btningresar);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            }
+        });
+    }
+}
