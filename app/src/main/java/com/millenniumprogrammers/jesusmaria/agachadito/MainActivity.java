eaecbd75791aa37db6eb093de541527b52661c24
CesarsEren <cpinedo428@gmail.com>
2019-12-03 16:48:56 -0500

    Diseño de Activity y Login|Main

--- app/src/main/java/com/millenniumprogrammers/jesusmaria/agachadito/MainActivity.java
+++ app/src/main/java/com/millenniumprogrammers/jesusmaria/agachadito/MainActivity.java
@@ -1,13 +1,39 @@
 package com.millenniumprogrammers.jesusmaria.agachadito;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import eu.long1.spacetablayout.SpaceTabLayout;
+
 public class MainActivity extends AppCompatActivity {
 
+    SpaceTabLayout tabLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        List<Fragment> fragmentList = new ArrayList<>();
+        fragmentList.add(new UCarritoFragment());
+        fragmentList.add(new UUbicacionesFragment());
+        fragmentList.add(new UComercioFragment());
+        fragmentList.add(new UTarjetasFragment());
+        fragmentList.add(new UPerfilFragment());
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        tabLayout = (SpaceTabLayout) findViewById(R.id.spaceTabLayout);
+
+        tabLayout.initialize(viewPager, getSupportFragmentManager(),
+                fragmentList, savedInstanceState);
+    }
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        tabLayout.saveState(outState);
+        super.onSaveInstanceState(outState);
     }
 }
--- app/src/main/java/com/millenniumprogrammers/jesusmaria/agachadito/UCarritoFragment.java
+++ app/src/main/java/com/millenniumprogrammers/jesusmaria/agachadito/UCarritoFragment.java
@@ -7,6 +7,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 
 /**
@@ -60,11 +66,71 @@ public class UCarritoFragment extends Fragment {
         }
     }
 
+    ListView lista;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ucarrito, container, false);
+        View view = inflater.inflate(R.layout.fragment_ucarrito, container, false);
+
+        lista = view.findViewById(R.id.lista);
+        //lista =
+        ArrayList<String> datos = new ArrayList<>();
+        datos.add("Cesars");
+        datos.add("Cesars");
+        datos.add("Cesars");
+        datos.add("Cesars");
+        datos.add("Cesars");
+        datos.add("Cesars");
+        datos.add("Cesars");
+        datos.add("Cesars");
+        datos.add("Cesars");
+        datos.add("Cesars");
+        datos.add("Cesars");
+        datos.add("Cesars");
+        datos.add("Cesars");
+        datos.add("Cesars");
+        datos.add("Cesars");
+        datos.add("Cesars");
+        datos.add("Cesars");
+        datos.add("Cesars");
+        datos.add("Cesars");
+        datos.add("Cesars");
+        datos.add("Cesars");
+        datos.add("Cesars");
+        datos.add("Cesars");
+        datos.add("Cesars");
+        datos.add("Cesars");
+        datos.add("Cesars");
+        datos.add("Cesars");
+        datos.add("Cesars");
+        datos.add("Cesars");
+        datos.add("Cesars");
+        datos.add("Cesars");
+        datos.add("Cesars");
+        datos.add("Cesars");
+        datos.add("Cesars");
+        datos.add("Cesars");
+        datos.add("Cesars");
+        datos.add("Cesars");
+        datos.add("Cesars");
+        datos.add("Cesars");
+        datos.add("Cesars");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity().getApplicationContext(), android.R.layout.simple_expandable_list_item_1,datos);
+        lista
+                .setAdapter(adapter);
+        lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getActivity().getApplicationContext(),String.valueOf(position),Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
